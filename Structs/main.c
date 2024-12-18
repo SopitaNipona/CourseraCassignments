cf02a81bd3004cb3a45848747deffd3ce8bf3826
SopitaNipona (Diego) <a01748692@tec.mx>
2024-12-18 00:24:45 -0600

    advance in enum typedef

--- Structs/main.c
+++ Structs/main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 
 typedef enum month {jan, feb, mar, apr, may, jun, jul, agu, sep, oct, nov, dec} month;
-typedef struct date {enum month m; int d;} date;
+typedef struct date {month m; int d;} date;
 
 month next_month(month m)
 {
     return(m + 1) % 12;
 }
 
-date next_date(month m, date da)
+date next_day(date *da)
 {
+    int day = da->d;
+    month m = da->m;
 
+    switch(m){
+    case m == feb && day == 28:
+        da -> d = 1;
+        da -> m = next_month(da -> m);
+        break;
+    default:
+        da -> d++;
+    }
+
+
+}
+
+date print_date(date *da)
+{
+    printf("%d/%d\n", da -> d, da -> m);
 }
 
 int main(void)
