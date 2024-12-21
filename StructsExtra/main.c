dc2180792d9fc6b4a37f4b049cd89f54334bc0e6
SopitaNipona (Diego) <a01748692@tec.mx>
2024-12-20 23:33:23 -0600

    cards

---
+++ StructsExtra/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef enum card_value {ace, one, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king} card_value;
+typedef enum card_color {red, black} card_color;
+typedef enum card_suit {} card_suit;
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
