48d6689ae0cbcdf75cee64ff48510ab0666a0335
SopitaNipona (Diego) <a01748692@tec.mx>
2024-12-21 10:28:25 -0600

    comments added

--- Structs/main.c
+++ Structs/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-//Enum and struct of month and date
+//Enum and struct of month and date defined into types
 
 typedef enum month {jan, feb, mar, apr, may, jun, jul, agu, sep, oct, nov, dec} month;
 typedef struct date {month m; int d;} date;
@@ -17,7 +17,7 @@ month next_month(month m)
 void next_day(date *da)
 {
 
-    //Switch statements to compare the end of each month
+    //Switch statements to indentify the end of each month
     switch(da -> m){
     case jan:
         if (da -> d == 31)
@@ -154,8 +154,6 @@ void next_day(date *da)
     default:
         da -> d++;
     }
-
-
 }
 
 //Prints the name of the month with the enum month number
@@ -168,21 +166,32 @@ void print_date(date *da)
 
 int main(void)
 {
+    //variable created with date type
     date test;
+    //February 28
     test.m = feb;
     test.d = 28;
+    //Next day and print date
     next_day(&test);
+    //date variable modified via pointer
+    //Print date
     print_date(&test);
+    //March 14
     test.m = mar;
     test.d = 14;
+    //Next day and print
     next_day(&test);
     print_date(&test);
+    //October 31
     test.m = oct;
     test.d = 31;
+    //Next day and print
     next_day(&test);
     print_date(&test);
+    //December 31
     test.m = dec;
     test.d = 31;
+    //Next day and print
     next_day(&test);
     print_date(&test);
 
