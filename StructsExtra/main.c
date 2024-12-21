e7d8f1f5c04b3c22949ad5eab404bafe1fa7ccbf
SopitaNipona (Diego) <a01748692@tec.mx>
2024-12-21 13:36:00 -0600

    array of pointers fixed

--- StructsExtra/main.c
+++ StructsExtra/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX_LEN 1000
+#define EMPTY (-1)
+#define FULL (MAX_LEN - 1)
 
 typedef enum card_value {ace, one, two, three, four, five, six, seven, eight, nine, ten, jack, queen, king} card_value;
 typedef enum card_suit {heart, diamonds, club, spade} card_suit;
+
+typedef struct card
+{
+    card_value value;
+    card_suit suit;
+} card;
+
+typedef struct card_stack
+{
+    card *data[];
+    int top;
+} card_stack;
+
+void reset(card_stack *stk){stk -> top = EMPTY;}
+
+void push(card_stack *stk, card *c_push)
+{
+    stk -> top++;
+    //check pointer
+    stk -> data[stk -> top] = c_push;
+}
+
+void make_card(){}
 
 int main(void)
 {
