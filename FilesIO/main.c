5b133fbd79e46f784cf15e93e90b8dba39c93eb6
SopitaNipona (Diego) <a01748692@tec.mx>
2025-01-17 00:49:43 -0600

    init FilesIO

--- FilesIO/main.c
+++ FilesIO/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int idk = 0;
+
 void print_file(FILE *fptr)
 {
     int c;
     rewind(fptr);
     while ((c = getc(fptr)) != EOF)
     {
-        putc(c, fptr);
+        putc(c, stdout);
     }
 }
 
 void generate_array(FILE *fptr)
 {
-    fscanf(fptr, "%d", );
+    fscanf(fptr, "%d", &idk);
 }
 
 int main(void)
 {
     FILE *fptr = fopen("test.txt", "r");
-    printf("Hello, World!\n");
+    print_file(fptr);
     return 0;
 }
