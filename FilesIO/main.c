4442d54580c9c04f4cf365e58775af0f19f2e63e
SopitaNipona (Diego) <a01748692@tec.mx>
2025-01-23 21:50:19 -0600

    commented code

--- FilesIO/main.c
+++ FilesIO/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Modified size global valued after running function generate_array
 int current_size = 0;
 
+//Prints the content of the file
 void print_file(FILE *fptr)
 {
     int c;
@@ -14,51 +16,62 @@ void print_file(FILE *fptr)
     }
 }
 
+//Returns a pointer with an int array
 int *generate_array(FILE *fptr)
 {
-
+    //gets the size value and place it initially at a char variable
     char sizechar;
     int size = 0;
     fscanf(fptr, "%s", &sizechar);
+    //char to int
     size = sizechar - '0';
+    //sets global variable
     current_size = size;
+    //2D char array to store the numbers using malloc
     char *arraychar = malloc(size * size * sizeof(char));
-    for (int i = 0; i < size; i++) {
-        //fscanf(fptr, "%s", &arraychar[i]);
-
+    //gets the data on a tempchar and then copy's it to the heap allocated char array
+    for (int i = 0; i < size; i++)
+    {
         char tempchar[size];
         fscanf(fptr, "%s", &tempchar);
         strcpy(&arraychar[i * size], tempchar);
     }
 
-    //printf("Array size: %s\n", arraychar[3]);
-    //printf("Array size: %s\n", &arraychar[3 * size]);
-
+    //Creates the int heap allocated array
     int *array = malloc(size * sizeof(int));
 
+    //converts the char numbers into int and inserts them into the int array
     for (int i = 0; i < size; i++)
     {
         array[i] = atoi(&arraychar[i * size]);
     }
 
+    //frees the char array
     free(arraychar);
 
+    //returns the heap int array
     return array;
 }
 
+//Gets the pointer and the global size value
 double average(int *array, int size)
 {
-    double sumatory = 0.0;
+    //Iterates the array and creates a summation
+    double summation = 0.0;
     for (int i = 0; i < size; i++)
     {
-        sumatory += array[i];
+        summation += array[i];
     }
-    return (sumatory / size);
+    //return the division between summation and size
+    return (summation / size);
 }
 
+//Gets the pointer and the global size value
 int max_element(int *array, int size)
 {
+    //Max as the initial value
     int max = array[0];
+    //iterates and compares the values to see if it is greater than the current value
     for (int i = 1; i < size; i++)
     {
         if (array[i] > max)
@@ -69,14 +82,19 @@ int max_element(int *array, int size)
     return max;
 }
 
+void print_array(int *array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
+
 int main(void)
 {
     FILE *fptr = fopen("test.txt", "r");
     int *array = generate_array(fptr);
-    for (int i = 0; i < current_size; i++)
-    {
-        printf("%d ", array[i]);
-    }
+    print_array(array, current_size);
     printf("\n");
     printf("%f\n", average(array, current_size));
     printf("%d\n", max_element(array, current_size));
